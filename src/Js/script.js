4d317d0c61e1c563222018c88cc02b45da7f2af4
ViteAAA <evmenovvitalj@gmail.com>
2024-09-22 23:29:12 +0300

    any blocks

--- src/Js/script.js
+++ src/Js/script.js
@@ -1,7 +1,57 @@
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
+    mousewheel: true,
     pagination: {
-      el: ".swiper-pagination",
+      el: ".mainp",
       clickable: true,
     },
-  });
+});
+
+// users slider
+var users = new Swiper(".users", {
+    cssMode: true,
+    direction: "horizontal",
+    navigation: {
+      nextEl: ".thirdbnf",
+      prevEl: ".thirdbpf",
+    },
+    pagination: {
+      el: ".thirdpf",
+    },
+});
+// organizations slider
+var org = new Swiper(".org", {
+  cssMode: true,
+  direction: "horizontal",
+  navigation: {
+    nextEl: ".thirdbns",
+    prevEl: ".thirdbps",
+  },
+  pagination: {
+    el: ".thirdps",
+  },
+});
+//comands and clubs
+var clubs = new Swiper(".clubs", {
+  cssMode: true,
+  direction: "horizontal",
+  navigation: {
+    nextEl: ".thirdbnt",
+    prevEl: ".thirdbpt",
+  },
+  pagination: {
+    el: ".thirdpt",
+  },
+});
+//arena
+var arena = new Swiper(".arena", {
+  cssMode: true,
+  direction: "horizontal",
+  navigation: {
+    nextEl: ".thirdbnfo",
+    prevEl: ".thirdbpfo",
+  },
+  pagination: {
+    el: ".thirdpfo",
+  },
+});
