2cd745f2b61a8657c8cf37b75e9ea2693e9b992b
ViteAAA <evmenovvitalj@gmail.com>
2024-09-23 17:44:51 +0300

    исправить ошибку со скрытием блоков

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '1/2': '50%',
+      }
+    },
   },
   plugins: [],
 }
