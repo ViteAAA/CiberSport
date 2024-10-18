97c32db3de49958668ac6336e1ca0b23b6274c00
ViteAAA <evmenovvitalj@gmail.com>
2024-10-18 15:24:34 +0300

    ...

--- src/Js/script.js
+++ src/Js/script.js
@@ -203,11 +203,22 @@ i.forEach(element => element.addEventListener("click", function() {
   swiper.slideTo(e);
 }))
 
-//hide header when footer
-// let slides = document.querySelectorAll(".swiper-slide");
-// slides.forEach(element => {
-//   if (element.classList.contains("footer"))
-//   {
-//     document.querySelector("header").hidden;
-//   }
-// });
+
+//фукция срабатывающая при взаимодействии с колесиком
+window.addEventListener('wheel', function(e) {
+  if (this.document.querySelector('.footer').classList.contains('swiper-slide-active')) {
+    document.querySelector('header').style.display = 'none';
+  } else {
+    document.querySelector('header').style.display = 'block';
+  }
+});
+
+const pagination = document.querySelectorAll('.mainp>span');
+// функция при нажатии на последний элемент списка
+pagination.forEach(element => element.addEventListener('click', function() {
+  if (pagination[pagination.length - 1] == element) {
+    document.querySelector('header').style.display = 'none';
+  } else {
+    document.querySelector('header').style.display = 'block';
+  }
+}));
